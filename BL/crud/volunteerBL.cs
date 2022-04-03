e9eaba5a4f78c77e5c2a01c9b4066cf688c725e0
BTUVGXHX <98056615+BTUVGXHX@users.noreply.github.com>
2022-04-03 21:39:13 +0300

    Add files via upload

--- BL/crud/volunteerBL.cs
+++ BL/crud/volunteerBL.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using DAL;
 using DTO;
 using System.Data.Entity;//כדי שיהיה ניתן להשתמש בנתונים גם אחרי  
+using System.Data.Entity.Migrations;
 
 namespace BL
 {
@@ -38,6 +39,48 @@ namespace BL
             }
         }
 
+        public static void updateVolunteer1(volunteerDTO volunteer)
+        {
+            using (VolunteerPlacementSystemDBEntities1 db = new VolunteerPlacementSystemDBEntities1())
+            {
+                var volunteerToUpdate = db.Volunteers.Find(volunteer.VolunteerID);
+                if (volunteerToUpdate != null)
+                {
+                    volunteerToUpdate.VolunteerFirstName = volunteer.VolunteerFirstName;
+                    volunteerToUpdate.VolunteerLastName = volunteer.VolunteerLastName;
+                    volunteerToUpdate.VolunteerGmail = volunteer.VolunteerGmail;
+                    volunteerToUpdate.VolunteerPassword = volunteer.VolunteerPassword;
+                    volunteerToUpdate.volunteerUsername = volunteer.volunteerUsername;
+                    volunteerToUpdate.VolunteerPhone = volunteer.VolunteerPhone;
+                    db.SaveChanges();
+                }
 
+            }
+        }
+
+        public static void UpdateVolunteer2(volunteerDTO volunteer)
+        {
+            using (VolunteerPlacementSystemDBEntities1 db = new VolunteerPlacementSystemDBEntities1())
+            {
+                db.Volunteers.AddOrUpdate(converts.volunteersConvert.convertVolunteerToDAL(volunteer));
+                db.SaveChanges();
+            }
+        }
+
+        public static bool CheckVolunteer(string id)
+        {
+            List<DAL.Volunteer> volunteer;
+            using (VolunteerPlacementSystemDBEntities1 db = new VolunteerPlacementSystemDBEntities1())
+            {
+                volunteer = (
+                    from v in db.Volunteers
+                    where v.VolunteerID.Equals(id)
+                    select v
+                      ).ToList();
+            }
+            if (volunteer.Count == 1)
+                return true;
+            return false;
+        }
     }
 }
